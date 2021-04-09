b193e5e039ba538b8f286280728288339fee218d
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-04-09 14:09:29 +0300

    Create API mock server

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react';
 import { MainLayout } from '../components/MainLayout';
 
 export default function Post() {
+
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		async function load() {
+			const response = await fetch('http://localhost:4200/posts')
+			const json = await response.json()
+			setPosts(json)
+		}
+		load()
+	}, [])
+
 	return (
 		<MainLayout title={"Posts Page"}>
 			<h1>Posts Page =)</h1>
+			<pre>{JSON.stringify(posts, null, 2)}</pre>
 		</MainLayout>
 	)
 }
