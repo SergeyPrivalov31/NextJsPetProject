47dace4932bbf75c1fa35b58d982911270eca9bb
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-04-09 15:59:29 +0300

    create posts

--- pages/posts.js
+++ pages/posts.js
@@ -1,22 +1,40 @@
 import { useState, useEffect } from 'react';
 import { MainLayout } from '../components/MainLayout';
+import Link from 'next/link';
 
-export default function Post() {
+export default function Post({ posts }) {
 
-	const [posts, setPosts] = useState([]);
-	useEffect(() => {
-		async function load() {
-			const response = await fetch('http://localhost:4200/posts')
-			const json = await response.json()
-			setPosts(json)
-		}
-		load()
-	}, [])
+	// const [posts, setPosts] = useState([]);
+	// useEffect(() => {
+	// 	async function load() {
+	// 		const response = await fetch('http://localhost:4200/posts')
+	// 		const json = await response.json()
+	// 		setPosts(json)
+	// 	}
+	// 	load()
+	// }, [])
 
 	return (
 		<MainLayout title={"Posts Page"}>
 			<h1>Posts Page =)</h1>
-			<pre>{JSON.stringify(posts, null, 2)}</pre>
+			<ul>
+				{posts.map(post => (
+					<li key={post.id}>
+						<Link href={`/post/[id]`} as={`/post/${post.id}`}>
+							<a>{post.title}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</MainLayout>
 	)
+}
+
+Post.getInitialProps = async () => {
+	const response = await fetch('http://localhost:4200/posts')
+	const posts = await response.json()
+
+	return {
+		posts
+	}
 }
