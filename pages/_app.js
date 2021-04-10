5fb83a2703da250aa3a237bfafa8f2426621e572
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-04-10 16:21:20 +0300

    Loading indicator. nextjs-progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/main.scss'
+import NextNprogress from 'nextjs-progressbar';
 
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<NextNprogress
+				color="yellow"
+				startPosition={0.3}
+				stopDelayMs={200}
+				height="3"
+			/>
 			<Component {...pageProps} />
 
 		</>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,24 @@
 import Router from 'next/router';
 import { MainLayout } from '../../components/MainLayout';
 
-export default function About() {
+export default function About({ title }) {
 	const linkToHomeHandler = () => {
 		Router.push('/')
 	}
 
 	return <MainLayout title={"About page"} >
-		<h1>About Page</h1>
+		<h1>{title}</h1>
+
 		<button onClick={linkToHomeHandler}>GO to Home page</button>
 		<button onClick={() => { Router.push('/posts') }}>GO to posts</button>
 	</MainLayout>
+}
+
+About.getInitialProps = async () => {
+	const response = await fetch('http://localhost:4200/about')
+	const data = await response.json()
+
+	return {
+		title: data.title
+	}
 }
